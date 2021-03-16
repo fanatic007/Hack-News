d3a878f1483559b315f53ea8be6e64295d557a5f
Sarang Tayde <sarang.tayde@gmail.com>
2021-03-17 02:01:11 +0530

    API server added

---
+++ server/server.js
@@ -0,0 +1,52 @@
+express = require('express');
+bodyParser = require('body-parser');
+Datastore = require('nedb');
+log=console.log;
+
+const ENDPOINT = '/challenges/';
+const app = express();
+const port = 3000;
+const challenges = new Datastore({ filename: 'challenges.db', autoload: true });
+
+app.use(bodyParser.json());
+
+//Get Challenges ordered by optional param
+app.get(ENDPOINT, (req, res) => {
+  challenges.find({},(err,result)=>{
+    if(err){log(err);
+      res.send({status:'failure'});
+    }
+    else{
+      res.send(result);
+    }
+  })
+});
+
+//Add Challenges
+app.post(ENDPOINT, (req, res) => {
+  challenges.insert(req.body,(err, result) => {
+    if(err){log(err);
+      res.send({status:'failure'});
+    }
+    else{
+      res.send({status:'success', id:result.id});
+    }
+  });
+});
+
+//Upvote Challenge (upvoted object to be sent)
+app.put(ENDPOINT,(req, res) => {
+  var {_id:id} = req.body;
+  challenges.update({_id:id},req.body,{},(err, result) => {
+    if(err){log(err);
+      res.send({status:'failure'});
+    }
+    else{
+      res.send({status:'success', id:result.id});
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Hack News server started and listening at http://localhost:${port}`)
+})
